659b0d8c1867c65018e48cb474164c2cefb08bea
hslee <hslee@idis.co.kr>
2017-07-20 15:04:50 +0900

    Version_0.0/

---
+++ Version_0.0/SoDA_Type.h
@@ -0,0 +1,17 @@
+#ifndef _SODA_TYPE_H
+#define _SODA_TYPE_H
+
+#if defined(_WIN32)
+#include <ctime>
+#endif
+
+namespace SoDA {
+    typedef   signed char  schar;
+    typedef unsigned char  uschar;
+    typedef   signed short sshort;
+    typedef unsigned short usshort;
+    typedef   signed int   sint;
+    typedef unsigned int   usint;
+}       // namespace SoDA
+
+#endif // _SODA_TYPE_H
